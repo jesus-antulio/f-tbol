64cf37559864518e7543aaa26df48cbe6260e510
jesus-antulio <@DonnaMarla1957>
2020-03-05 15:05:24 -0600

    Add MasajistaHM

---
+++ src/learning/MasajistaHM.java
@@ -0,0 +1,16 @@
+package learning;
+
+public class MasajistaHM extends SeleccionFutbolHM implements IntegranteInterface {
+    private String titulacion;
+    private int aniosExperiencia;
+
+    @Override
+    public void concentrarse() {
+
+    }
+
+    @Override
+    public void viajar() {
+
+    }
+}
