ee69e66191b021f4a0a42bdae759f8674a5422f5
jesus-antulio <@DonnaMarla1957>
2020-03-05 15:03:25 -0600

    Add EntrenadorHM

---
+++ src/learning/EntrenadorHM.java
@@ -0,0 +1,25 @@
+package learning;
+
+public class EntrenadorHM extends SeleccionFutbolHM implements DeportistaInterface, IntegranteInterface{
+    private int idFederacion;
+
+    @Override
+    public void entrenar() {
+
+    }
+
+    @Override
+    public void jugarPartido() {
+
+    }
+
+    @Override
+    public void concentrarse() {
+
+    }
+
+    @Override
+    public void viajar() {
+
+    }
+}
