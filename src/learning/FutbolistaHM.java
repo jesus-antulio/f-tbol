44be6444432d84019e0036a7dd7fefd03c3ed426
jesus-antulio <@DonnaMarla1957>
2020-03-05 08:56:15 -0600

    Add FutbolistaHM

---
+++ src/learning/FutbolistaHM.java
@@ -0,0 +1,26 @@
+package learning;
+
+public class FutbolistaHM extends SeleccionFutbolHM implements DeportistaInterface, IntegranteInterface{
+    private int dorsal;
+    private String demarcacion;
+
+    @Override
+    public void entrenar() {
+
+    }
+
+    @Override
+    public void jugarPartido() {
+
+    }
+
+    @Override
+    public void concentrarse() {
+
+    }
+
+    @Override
+    public void viajar() {
+
+    }
+}
